02a4f85039d9fb2426cc5e22f9c812edb2dd800b
TakeOff1-dev <t10278988@gmail.com>
2025-10-01 21:38:37 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -266,7 +266,13 @@ function Home({ addToCard }) {
               exercitationem consequatur <br />
               reprehenderit voluptatum ab quibusdam
               delectus, dolores alias eligendi
-              dolore itaque repudiandae cum ipsa. </i> </p>
+              dolore itaque repudiandae cum ipsa. </i> 
+              <div class="custom-shape-divider-top-1759346447">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
+              </p>
             <div className="w-3/4 m-auto">
               <div className="mt-20">
                 <Slider {...settings}>
