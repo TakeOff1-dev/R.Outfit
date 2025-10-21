45d662175e93154a3609a1564afcbf5940127ed1
TakeOff1-dev <t10278988@gmail.com>
2025-10-21 22:13:34 +0200

    update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,107 +1,116 @@
-import '../css/Cart.css'
-import {Link }from 'react-router-dom'
+import '../css/Cart.css';
+import { Link } from 'react-router-dom';
 
 export default function Cart({ cart, setCard }) {
 
-    // remove from cart
-    const remove = ({cart, item}) => {
-        alert("Item removed from cart. ");
-        const allItems = [...cart] //getting all items from the card
-        allItems.splice((item.id - 1), 1) //the 1) is to delete a single
-        setCard(allItems)
-        // there is a function (suplice) to delete the  item from card
+    // Remove item from cart
+    const remove = (item) => {
+        alert("Item removed from cart.");
+        const updatedCart = cart.filter((cartItem) => cartItem.id !== item.id);
+        setCard(updatedCart);
+    };
 
-    }
-
-    // add quantity function
+    // Increase quantity
     const addQuantity = (item) => {
-        // alert("quantity added");
-        const allItems = [...cart]
-        console.log(allItems);
-        allItems[item.id - 1].quantity += 1
-        setCard(allItems);
+        const updatedCart = cart.map(cartItem =>
+            cartItem.id === item.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+        setCard(updatedCart);
+    };
 
-
-    }
-
-    //  minus  quantity function
+    // Decrease quantity
     const reduceQuantity = (item) => {
-        // alert("quantity reduced");
-        const allItems = [...cart]
-        allItems[item.id - 1].quantity -= 1
-        setCard(allItems)
-    }
+        if (item.quantity > 1) {
+            const updatedCart = cart.map(cartItem =>
+                cartItem.id === item.id
+                    ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                    : cartItem
+            );
+            setCard(updatedCart);
+        } else {
+            remove(item);
+        }
+    };
+
+    // Calculate total price and order summary
+    let totalPrice = 0;
+    const orderSummary = cart.map((item) => {
+        const itemTotal = item.quantity * item.price;
+        totalPrice += itemTotal;
+
+        return (
+            <div key={item.id}>
+                <div className="subtotal">
+                    <h5>{item.title.substring(0, 12)}</h5>
+                    <p>R {itemTotal.toFixed(2)}</p>
+                    <h6>Total Q'ty: {item.quantity}</h6>
+                </div>
+                <hr />
+            </div>
+        );
+    });
 
     return (
         <>
-            < br /> < br /> < br /> < br />  < br /><br /> 
+            <br /><br /><br /><br /><br /><br />
 
+            <div className="container">
+                <div className="row">
+                    {/* Left: Cart Items */}
+                    <div className="col-md-8">
+                        <br />
+                        <div className="d-flex">
+                            <p>Your cart <Link to="/Shipping"> → Shipping</Link></p>
+                        </div>
 
-            {
-                <div className="container" >
-<div className="row">
-    <div className="col-md-8">
-                    <br />
-                    <div className="d-flex">
-                 <p>Your cart <Link to="/Shipping"> Shipping  </Link></p>
-                   
-                    </div>
-                 
-                    
-                    {
-                        cart.map(item => (
-                            <div key={item.id} className="addToCard">
-                                <img src={item.image} alt="" />
-                                <h5>{item.title}</h5>
-                                <p>{item.price * item.quantity}</p>
-                                <div className="btn">
-                                    <button onClick={() => addQuantity(item)}>+</button>
-                                    <button >{item.quantity}</button> {/*was added in json  for product qunatity*/}
-                                    <button onClick={() => reduceQuantity(item)}>-</button>
-                                    <div className=" btn btn-danger" onClick={() => remove(item)}>Remove</div>
+                        {cart.length === 0 ? (
+                            <p>Your cart is empty.</p>
+                        ) : (
+                            cart.map((item) => (
+                                <div key={item.id} className="addToCard">
+                                    <img src={item.image} alt={item.title} />
+                                    <h5>{item.title}</h5>
+                                    <p>R {(item.price * item.quantity).toFixed(2)}</p>
+                                    <div className="btn">
+                                        <button onClick={() => addQuantity(item)}>+</button>
+                                        <button>{item.quantity}</button>
+                                        <button onClick={() => reduceQuantity(item)}>-</button>
+                                        <div className="btn btn-danger" onClick={() => remove(item)}>
+                                            Remove
+                                        </div>
+                                    </div>
                                 </div>
+                            ))
+                        )}
+                    </div>
 
-                            </div>
-                        ))
-                    }
+                    {/* Right: Order Summary */}
+                    <div className="col-md-4" id="shipping-bg">
+                        <br />
+                        <h4>Order Summary</h4>
+                        <br />
+                        {orderSummary}
+                        <h6>Total No of Items: {cart.length}</h6>
+                        <h5><p>Total Price: R {totalPrice.toFixed(2)}</p></h5>
+
+                        <br /><br />
+                        <Link to="/Shipping">
+                            <button id="btn" style={{ width: "80%" }}>
+                                Proceed to checkout
+                            </button>
+                        </Link>
+                        <br /><br />
+
+                        <Link to="/">
+                            <button id="btnn" style={{ width: "80%" }}>
+                                Continue shopping
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-                 
-                <div className="col-md-4" id="shipping-bg">
-                    <br />
-                   <h4>Order Summary</h4> 
-                   <br />
-                 
-{
-    cart.map(item =>(
-         <div key={item.id} >
-  <div className="subtotal">
-                    <h5>{item.title.substring(0, 12)} </h5> 
-                    <p>R {item.price * item.quantity}</p> 
-                <h6> Total Q'ty  :{  item.quantity} </h6>
-                   </div> 
-                   <hr />
-     </div>
-     
-    ))
-   
-}
-
-<h6> Total No of items :{ cart.length } </h6>
-
-<br /><br />
-           <Link to="/Shipping"><button id='btn' style={{width:"80%"}}>Proceed to checkout</button><br /><br />    </Link>
-
-            <Link to="/"> <button id='btnn' style={{width:"80%"}}>Continue shopping</button><br /><br /> </Link>  
-               
-            
-                </div>
-</div>
-</div>
-
-  
-            }
-
-
+            </div>
         </>
-    )
+    );
 }
--- src/components/Shipping.jsx
+++ src/components/Shipping.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
-export default function Shipping() {
+export default function Shipping({ cart, setCard }) {
 
     // ## Storing input values
 
@@ -48,6 +48,8 @@ export default function Shipping() {
         }
 
     }
+
+
     return (
         <>
             < br /> < br /> < br /> < br />  < br /><br /><br /><br />
@@ -57,7 +59,7 @@ export default function Shipping() {
 
                     <div className="row">
 
-                        <div className="col-md-6">
+                        <div className="col-md-4">
 
                             <div className="form-floating mb-3">
                                 <input type="text" className="form-control" id="floatingInput" placeholder="firstname"
@@ -68,49 +70,56 @@ export default function Shipping() {
                                 <input type="text" className="form-control" id="floatingInput" placeholder="lastname" name="lastname" onChange={handleChange} required />
                                 <label for="floatingInput">Last name</label>
                             </div>
+                            <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="floatingInput" placeholder="state" name="state" onChange={handleChange} required />
+                                    <label for="floatingInput">State</label>
+                                </div>
+                                 <div className="form-floating mb-3">
+                                    <input type="number" className="form-control" id="floatingInput" placeholder="zipcode" name="zipcode" onChange={handleChange} required />
+                                    <label for="floatingInput">Zip code</label>
+                                </div>
 
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-4">
                             <div className="form-floating mb-3">
                                 <input type="email" className="form-control" id="floatingInput" placeholder="email" name="email" onChange={handleChange} required />
                                 <label for="floatingInput">E-mail</label>
+
+                                 <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="floatingInput" placeholder="city" name="city" onChange={handleChange} required />
+                                    <label for="floatingInput">City</label>
+                                </div>
                             </div>
                             <div className="form-floating mb-3">
                                 <input type="number" className="form-control" id="floatingInput" placeholder="number" name="phone" onChange={handleChange} required />
                                 <label for="floatingInput">Phone number</label>
                             </div>
-
-                        </div>
-                        {/* another row */}
-                        <div className="row">
-                            <div className="col-md-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" className="form-control" id="floatingInput" placeholder="city" name="city" onChange={handleChange} required />
-                                    <label for="floatingInput">City</label>
-                                </div>
-                            </div>
-                            <div className="col-md-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" className="form-control" id="floatingInput" placeholder="state" name="state" onChange={handleChange} required />
-                                    <label for="floatingInput">State</label>
-                                </div>
-                            </div>
-                            <div className="col-md-4">
-                                <div className="form-floating mb-3">
-                                    <input type="number" className="form-control" id="floatingInput" placeholder="zipcode" name="zipcode" onChange={handleChange} required />
-                                    <label for="floatingInput">Zip code</label>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* last row */}
-
-                        <div className="form-floating mb-3" >
-                            <input type="textarea" style={{ padding: "100px" }} className="form-control" id="floatingInput" placeholder="description" name="description" onChange={handleChange} />
+                            <div className="form-floating mb-3" >
+                            <input type="textarea" style={{ padding: "60px" }} className="form-control" id="floatingInput" placeholder="description" name="description" onChange={handleChange} />
                             <label for="floatingInput">Desciprion</label>
                         </div>
 
-                        <Link to="/Payment"> <button onClick={handleSubmit} type="submit" id='btn' style={{ width: "90%" }}>Continue payment</button><br /><br /></Link>
+                        </div>
+
+                         {/* card */}
+                        <div className="col md 4">
+                            <p>Card</p>
+
+ {/* Right: Order Summary */}
+                   
+
+                            
+                             <Link to="/Payment"> <button onClick={handleSubmit} type="submit" id='btn' style={{ width: "90%" }}>Continue payment</button><br /><br /></Link>
+                             
+                        </div>
+
+                      
+
+                        {/* last row */}
+
+                        
+
+                       
                     </div>
                 </div>
             </form>
